1e73bec4a2480f691a7a22a6003278a3aff10f0c
Luke Saxton <luke@nu-desine.com>
2017-01-18 15:48:24 +0000

    Begin adding placeholder classes.

---
+++ GUI/VirtualMidiController.cpp
@@ -0,0 +1,9 @@
+//
+//  VirtualMidiController.cpp
+//  AIAO
+//
+//  Created by Luke Saxton on 17/01/2017.
+//
+//
+
+#include "VirtualMidiController.hpp"
---
+++ GUI/VirtualMidiController.hpp
@@ -0,0 +1,24 @@
+//
+//  VirtualMidiController.hpp
+//  AIAO
+//
+//  Created by Luke Saxton on 17/01/2017.
+//
+//
+
+#ifndef VirtualMidiController_hpp
+#define VirtualMidiController_hpp
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+class VirtualMidiController : public Component
+{
+public:
+    VirtualMidiController();
+    ~VirtualMidiController();
+private:
+    
+};
+
+
+#endif /* VirtualMidiController_hpp */
