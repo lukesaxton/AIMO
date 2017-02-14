96cdce39dd7b1318f30bf73dc556ee2117933bad
Luke Saxton <luke@nu-desine.com>
2017-02-14 20:42:21 +0000

    Fix Typo in License

--- MainComponent.cpp
+++ MainComponent.cpp
@@ -2,7 +2,7 @@
 //  it under the terms of the GNU General Public License, version 3,
 //  as published by the Free Software Foundation.
 //
-//  AI::MO is distributed in the hope that it will be useful,
+//  AI:MO is distributed in the hope that it will be useful,
 //  but WITHOUT ANY WARRANTY; without even the implied warranty of
 //  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //  GNU General Public License for more details.
