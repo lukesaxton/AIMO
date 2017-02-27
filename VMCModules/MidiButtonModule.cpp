e734bb221a47c86cb7e51f3994ef706190ee7f05
Luke Saxton <luke@nu-desine.com>
2017-02-27 16:43:33 +0000

    Multi-Purpose Midi Button

---
+++ VMCModules/MidiButtonModule.cpp
@@ -0,0 +1,139 @@
+//
+//  MidiButtonModule.cpp
+//  AIMO
+//
+//  Created by Luke Saxton on 27/02/2017.
+//
+//
+//  AI:MO is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License, version 3,
+//  as published by the Free Software Foundation.
+//
+//  AI:MO is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "MidiButtonModule.hpp"
+
+
+MidiButtonModule::MidiButtonModule()
+{
+    onColour = Colours::snow;
+    offColour = Colours::grey;
+}
+
+MidiButtonModule::~MidiButtonModule()
+{
+    
+}
+
+void MidiButtonModule::paint(Graphics& g)
+{
+    float padding = getWidth()*0.0375;
+    g.setColour(Colours::darkgrey);
+    g.drawRect(padding, padding, getWidth()-1.5*padding, getHeight() -1.5*padding);
+    
+    
+    if (buttonState == 1)
+    {
+        g.setColour(onColour);
+    }
+    else
+    {
+        g.setColour(offColour);
+    }
+    
+    g.fillRect(padding, padding, getWidth()- 2*padding, getHeight() - 2*padding);
+    
+    
+}
+void MidiButtonModule::resized()
+{
+    
+}
+
+void MidiButtonModule::setButtonState(const bool newState)
+{
+    buttonState = newState;
+    triggerAsyncUpdate();
+}
+
+void MidiButtonModule::setColour(const Colour newColour)
+{
+    onColour = newColour;
+    offColour = newColour.darker().darker();
+    triggerAsyncUpdate();
+}
+
+const bool MidiButtonModule::getButtonState()
+{
+    return buttonState;
+}
+
+
+void MidiButtonModule::processMidi (MidiMessage* message)
+{
+    if (message)
+    {
+        if (buttonMode == Standard)
+        {
+            buttonState = message->isNoteOn();
+            triggerAsyncUpdate();
+        }
+        else if (buttonMode == Toggle)
+        {
+            if (message->isNoteOnOrOff())
+            {
+                if (message->isNoteOn())
+                {
+                    buttonState = !buttonState;
+                    if (!buttonState)
+                    {
+                        message->setVelocity(0);
+                    }
+                    triggerAsyncUpdate();
+                    
+                }
+            }
+        }
+        else if (buttonMode == ToggleCC)
+        {
+            if (message->isNoteOnOrOff())
+            {
+                if (message->isNoteOn())
+                {
+                    buttonState = !buttonState;
+                    if (!buttonState)
+                    {
+                        message->setVelocity(0);
+                    }
+                    triggerAsyncUpdate();
+                }
+            }
+        }
+        else if (buttonMode == List)
+        {
+            //Not implemented yet
+            jassertfalse;
+        }
+    }
+}
+
+const int MidiButtonModule::getButtonMode()
+{
+    return buttonMode;
+}
+
+bool MidiButtonModule::setButtonMode(const int newMode)
+{
+    if (newMode > 0 && newMode < FINAL_BUTTONMODE)
+    {
+        buttonMode = ButtonMode(newMode);
+        return true;
+    }
+    return false;
+}
---
+++ VMCModules/MidiButtonModule.hpp
@@ -0,0 +1,115 @@
+//
+//  MidiButtonModule.hpp
+//  AIMO
+//
+//  Created by Luke Saxton on 27/02/2017.
+//
+//
+//  AI:MO is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License, version 3,
+//  as published by the Free Software Foundation.
+//
+//  AI:MO is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef MidiButtonModule_hpp
+#define MidiButtonModule_hpp
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "VMCModules.hpp"
+
+class MidiButtonModule : public VMCAsyncControl,
+                         public VMCMidiProcessor
+{
+public:
+    enum ButtonMode{
+        Standard = 1,
+        Toggle,
+        ToggleCC,
+        List,
+        FINAL_BUTTONMODE
+    };
+    
+    class ConfigComponent : public Component,
+    public Slider::Listener,
+    public ComboBox::Listener
+    {
+    public:
+        ConfigComponent(MidiButtonModule* _button)
+        {
+            
+            midiButton = _button;
+            setSize(200, 300);
+            
+            modeBox.addItem("Standard", ButtonMode::Standard);
+            modeBox.addItem("Toggle", ButtonMode::Toggle);
+            modeBox.addItem("ToggleCC", ButtonMode::ToggleCC);
+            modeBox.addItem("List", ButtonMode::List);
+
+            modeBox.setSelectedId(midiButton->getButtonMode(), dontSendNotification);
+            modeBox.addListener(this);
+            addAndMakeVisible(modeBox);
+            
+            
+            
+        }
+        ~ConfigComponent()
+        {
+            
+        }
+        void paint(Graphics& g) override
+        {
+            //g.fillAll(Colours::darkgrey);
+        }
+        void resized() override
+        {
+            modeBox.setBounds(0, 0, getWidth(), 30);
+            
+        }
+        
+        void sliderValueChanged (Slider* slider) override
+        {
+
+        }
+        
+        void comboBoxChanged (ComboBox* comboBoxThatHasChanged) override
+        {
+            if (comboBoxThatHasChanged == &modeBox)
+            {
+                midiButton->setButtonMode(modeBox.getSelectedId());
+            }
+
+        }
+        
+    private:
+        MidiButtonModule* midiButton;
+        ComboBox modeBox;
+    };
+    
+    
+    MidiButtonModule();
+    ~MidiButtonModule();
+    void paint(Graphics& g) override;
+    void resized() override;
+    void setButtonState(const bool newState);
+    void setColour(const Colour newColour);
+    const bool getButtonState();
+    void processMidi (MidiMessage* message) override;
+    
+    const int getButtonMode();
+    bool setButtonMode(const int newMode);
+    
+    
+private:
+    bool buttonState = 0;
+    Colour onColour, offColour;
+    String mapOut;
+    ButtonMode buttonMode = Standard;
+};
+
+#endif /* MidiButtonModule_hpp */
